fbf1e617e973e8aa5f9fceefcadcf2d74741b1eb
bhumika2326 <bhumikavranwani.bscibm2024@iar.ac.in>
2025-02-13 16:02:58 +0530

    Update temperature.py

--- temperature.py
+++ temperature.py
@@ -1,4 +1,5 @@
-#python
+## python code for conversion of temperature from fahrenheit to celsius.
+print("This is code for conversion of temperature from fahrenheit to degree celsius")
 ## Taking input from the user
 Temperature = float (input("Enter temperature in Fahrenheit:     "))
 ## Converting Fahrenheit into Celsius
