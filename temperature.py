83f8b1b2d2a477e0e35e20d30ca8f6d0c855d181
Bhumika <bhumikavranwani.bscibm2024@iar.ac.in>
2025-02-13 11:05:44 +0530

    Conversion of temperature from fahrenheit to celsius

---
+++ temperature.py
@@ -0,0 +1,6 @@
+#python
+## Taking input from the user
+Temperature = float (input("Enter temperature in Fahrenheit:     "))
+## Converting Fahrenheit into Celsius
+Celsius = (Temperature - 32) * 5/9
+print ("Temperature in Celsius:      ",Celsius)
